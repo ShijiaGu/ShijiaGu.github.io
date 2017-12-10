f8c4c72ac1b45500d7f3d06735ad5bafad5a464f
Shijia <gushijia920@gmail.com>
2017-12-10 12:52:59 -0500

    Homework-startup-matchmaker

---
+++ Class-Work/class07/first-javascript/script.js
@@ -0,0 +1,29 @@
+console.log('this a sweet status update');
+
+var product = 10 * 10;
+
+var quotient = 100 / 10;
+
+var difference = 50 - 17;
+
+var sum = 50 + 17;
+
+var name = 'Shijia'+' Gu'
+
+console.log(product, quotient, difference, sum, name);
+
+
+function onra () {
+  var city = 'Saigon'
+  console.log (`Last Tango In ${city}`)
+}
+
+onra();
+onra();
+onra();
+
+// console.log(document.getElementById('hello'));
+
+var hello = document.getElementById('hello');
+
+hello.style.color = 'tomato';
